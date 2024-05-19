da311d0b68185bc5441366bb837ff6403c8bb408
MyNameIsTrez <welfje@gmail.com>
2024-05-19 19:29:17 +0200

    Don't only push hardcoded symbols

--- generate_complex_so.c
+++ generate_complex_so.c
@@ -56,6 +56,8 @@ static size_t symbols_size;
 static char *shuffled_symbols[MAX_SYMBOLS];
 static size_t shuffled_symbols_size;
 
+static u32 buckets[MAX_BUCKETS];
+
 static u32 chains[MAX_SYMBOLS];
 static size_t chains_size;
 
@@ -84,7 +86,7 @@ static void push_string(char *str) {
     push_byte('\0');
 }
 
-static void push_shstrtab() {
+static void push_shstrtab(void) {
     push_byte(0);
     push_string(".symtab");
     push_string(".strtab");
@@ -98,11 +100,13 @@ static void push_shstrtab() {
     push_zeros(2);
 }
 
-static void push_strtab() {
+static void push_strtab(void) {
     push_byte(0);
     push_string("foo.s");
     push_string("_DYNAMIC");
-    push_string("foo");
+    for (size_t i = 0; i < symbols_size; i++) {
+        push_string(shuffled_symbols[i]);
+    }
 }
 
 static void push_number(u64 n, size_t byte_count) {
@@ -130,7 +134,7 @@ static void push_symbol_entry(u32 name, u32 value, u32 size, u32 info, u32 other
     push_number(shndx, 4);
 }
 
-static void push_symtab() {
+static void push_symtab(void) {
     // TODO: Some of these can be turned into enums using https://docs.oracle.com/cd/E19683-01/816-1386/chapter6-79797/index.html
     push_symbol_entry(0, 0, 0, 0, 0, 0); // "<null>"
     push_symbol_entry(1, 0xfff10004, 0, 0, 0, 0); // "foo.s"
@@ -139,7 +143,7 @@ static void push_symtab() {
     push_symbol_entry(16, 0x60010, 0x2000, 0, 0, 0); // "foo"
 }
 
-static void push_data() {
+static void push_data(void) {
     push_string("bar");
     push_zeros(4);
 }
@@ -164,18 +168,20 @@ static void push_dynamic() {
     push_dynamic_entry(DT_NULL, 0);
 }
 
-static void push_dynstr() {
+static void push_dynstr(void) {
     push_byte(0);
-    push_string("foo");
+    for (size_t i = 0; i < symbols_size; i++) {
+        push_string(symbols[i]);
+    }
 }
 
-static void push_dynsym() {
+static void push_dynsym(void) {
     // TODO: Some of these can be turned into enums using https://docs.oracle.com/cd/E19683-01/816-1386/chapter6-79797/index.html
     push_symbol_entry(0, 0, 0, 0, 0, 0); // "<null>"
     push_symbol_entry(1, 0x60010, 0x2000, 0, 0, 0); // "foo"
 }
 
-static u32 get_nbucket() {
+static u32 get_nbucket(void) {
     // From https://sourceware.org/git/?p=binutils-gdb.git;a=blob;f=bfd/elflink.c;h=6db6a9c0b4702c66d73edba87294e2a59ffafcf5;hb=refs/heads/master#l6560
     //
     // Array used to determine the number of hash table buckets to use
@@ -247,8 +253,8 @@ static void push_chain(u32 chain) {
 //  0  <STN_UNDEF>       |
 //  1  b                 |  98             2                 /---> 0
 //  2  p                 | 112             1                 | /-> 0
-//  3  j                 | 106             1                 | \-- 2 <---\
-//  4  n                 | 110             2                 \---- 1 <---|-\
+//  3  j                 | 106             1                 | \-- 2 <---\.
+//  4  n                 | 110             2                 \---- 1 <---|-\.
 //  5  f                 | 102             0                       0 <-\ | |
 //  6  g                 | 103             1                 /---> 3 --|-/ |
 //  7  o                 | 111             0                 | /-> 5 --/   |
@@ -256,19 +262,17 @@ static void push_chain(u32 chain) {
 //  9  k                 | 107             2               /-|---> 4 --|---/
 // 10  i                 | 105             0               | | /-> 8 --/
 // 11  c                 |  99             0 **            | | \-(10)
-// 12  d                 | 100             1               | \---- 6 <-\
-// 13  h                 | 104             2               \------ 9 <-|-\
+// 12  d                 | 100             1               | \---- 6 <-\.
+// 13  h                 | 104             2               \------ 9 <-|-\.
 // 14  a                 |  97             1                  /-> 12 --/ |
 // 15  e                 | 101             2 **               |  (13)----/
 // 16  m                 | 109             1 **               \--(14)
-static void push_hash() {
+static void push_hash(void) {
     u32 nbucket = get_nbucket();
     push_number(nbucket, 4);
 
     u32 nchain = 1 + symbols_size; // `1 + `, because index 0 is always STN_UNDEF (the value 0)
     push_number(nchain, 4);
-
-    static u32 buckets[MAX_BUCKETS];
 
     memset(buckets, 0, nbucket * sizeof(u32));
 
@@ -296,7 +300,7 @@ static void push_hash() {
     // push_zeros(4); // Alignment
 }
 
-static void push_section(u32 name_offset, u32 type, u64 flags, u64 address, u64 offset, u64 size, u32 link, u32 info, u64 alignment, u64 entry_size) {
+static void push_section_header(u32 name_offset, u32 type, u64 flags, u64 address, u64 offset, u64 size, u32 link, u32 info, u64 alignment, u64 entry_size) {
     push_number(name_offset, 4);
     push_number(type, 4);
     push_number(flags, 8);
@@ -309,48 +313,48 @@ static void push_section(u32 name_offset, u32 type, u64 flags, u64 address, u64 
     push_number(entry_size, 8);
 }
 
-static void push_section_headers() {
+static void push_section_headers(void) {
     // Null section
     // 0x20e0 to 0x2120
     push_zeros(0x40);
 
     // .hash: Hash section
     // 0x2120 to 0x2160
-    push_section(0x1b, SHT_HASH, SHF_ALLOC, 0x120, 0x120, 0x14, 2, 0, 8, 4);
+    push_section_header(0x1b, SHT_HASH, SHF_ALLOC, 0x120, 0x120, 0x14, 2, 0, 8, 4);
 
     // .dynsym: Dynamic linker symbol table section
     // 0x2160 to 0x21a0
-    push_section(0x21, SHT_DYNSYM, SHF_ALLOC, 0x138, 0x138, 0x30, 3, 1, 8, 0x18);
+    push_section_header(0x21, SHT_DYNSYM, SHF_ALLOC, 0x138, 0x138, 0x30, 3, 1, 8, 0x18);
 
     // .dynstr: String table section
     // 0x21a0 to 0x21e0
-    push_section(0x29, SHT_STRTAB, SHF_ALLOC, 0x168, 0x168, 5, 0, 0, 1, 0);
+    push_section_header(0x29, SHT_STRTAB, SHF_ALLOC, 0x168, 0x168, 5, 0, 0, 1, 0);
 
     // .eh_frame: Program data section
     // 0x21e0 to 0x2220
-    push_section(0x31, SHT_PROGBITS, SHF_ALLOC, 0x1000, 0x1000, 0, 0, 0, 8, 0);
+    push_section_header(0x31, SHT_PROGBITS, SHF_ALLOC, 0x1000, 0x1000, 0, 0, 0, 8, 0);
 
     // .dynamic: Dynamic linking information section
     // 0x2220 to 0x2260
-    push_section(0x3b, SHT_DYNAMIC, SHF_WRITE | SHF_ALLOC, 0x1f50, 0x1f50, 0xb0, 3, 0, 8, 0x10);
+    push_section_header(0x3b, SHT_DYNAMIC, SHF_WRITE | SHF_ALLOC, 0x1f50, 0x1f50, 0xb0, 3, 0, 8, 0x10);
 
     // .data: Data section
     // 0x2260 to 0x22a0
-    push_section(0x44, SHT_PROGBITS, SHF_WRITE | SHF_ALLOC, 0x2000, 0x2000, 4, 0, 0, 4, 0);
+    push_section_header(0x44, SHT_PROGBITS, SHF_WRITE | SHF_ALLOC, 0x2000, 0x2000, 4, 0, 0, 4, 0);
 
     // .symtab: Symbol table section
     // 0x22a0 to 0x22e0
     // "link" of 8 is the section header index of the associated string table; see https://blog.k3170makan.com/2018/09/introduction-to-elf-file-format-part.html
     // "info" of 4 is one greater than the symbol table index of the last local symbol (binding STB_LOCAL)
-    push_section(1, SHT_SYMTAB, 0, 0, 0x2008, 0x78, 8, 4, 8, 0x18);
+    push_section_header(1, SHT_SYMTAB, 0, 0, 0x2008, 0x78, 8, 4, 8, 0x18);
 
     // .strtab: String table section
     // 0x22e0 to 0x2320
-    push_section(0x09, SHT_PROGBITS | SHT_SYMTAB, 0, 0, 0x2080, 0x14, 0, 0, 1, 0);
+    push_section_header(0x09, SHT_PROGBITS | SHT_SYMTAB, 0, 0, 0x2080, 0x14, 0, 0, 1, 0);
 
     // .shstrtab: Section header string table section
     // 0x2320 to end
-    push_section(0x11, SHT_PROGBITS | SHT_SYMTAB, 0, 0, 0x2094, 0x4a, 0, 0, 1, 0);
+    push_section_header(0x11, SHT_PROGBITS | SHT_SYMTAB, 0, 0, 0x2094, 0x4a, 0, 0, 1, 0);
 }
 
 static void push_program_header(u32 type, u32 flags, u64 offset, u64 virtual_address, u64 physical_address, u64 file_size, u64 mem_size, u64 alignment) {
@@ -364,7 +368,7 @@ static void push_program_header(u32 type, u32 flags, u64 offset, u64 virtual_add
     push_number(alignment, 8);
 }
 
-static void push_elf_header() {
+static void push_elf_header(void) {
     // Magic number
     // 0x0 to 0x4
     push_byte(0x7f);
@@ -523,7 +527,7 @@ static unsigned long bfd_hash_hash(const char *string) {
 // "a"
 // "e"
 // "m"
-static void generate_shuffled_symbols() {
+static void generate_shuffled_symbols(void) {
     // See the documentation above push_hash() for how this function works
 
     #define DEFAULT_SIZE 4051 // From https://sourceware.org/git/?p=binutils-gdb.git;a=blob;f=bfd/hash.c#l345
@@ -573,14 +577,14 @@ static void push_symbol(char *symbol) {
     symbols[symbols_size++] = symbol;
 }
 
-static void reset() {
+static void reset(void) {
     symbols_size = 0;
     shuffled_symbols_size = 0;
     chains_size = 0;
     bytes_size = 0;
 }
 
-static void generate_simple_so() {
+static void generate_simple_so(void) {
     reset();
 
     FILE *f = fopen("foo.so", "w");
@@ -618,7 +622,7 @@ static void generate_simple_so() {
     generate_shuffled_symbols();
 
     // 0x120 to 0x134
-    push_hash(shuffled_symbols);
+    push_hash();
 
     // 0x138 to 0x168
     push_dynsym();
@@ -654,6 +658,6 @@ static void generate_simple_so() {
     fclose(f);
 }
 
-int main() {
+int main(void) {
     generate_simple_so();
 }
